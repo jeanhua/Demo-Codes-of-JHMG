8695864f17e5136b987594d3f72e5e3480a9ad1f
jeanhua <2207739460@qq.com>
2024-05-24 22:41:15 +0800

    Add files via upload

--- 基于JHMG的简单计算器/基于JHMG的简单计算器.cpp
+++ 基于JHMG的简单计算器/基于JHMG的简单计算器.cpp
@@ -1,5 +1,5 @@
 #include "JHMG engine.h"
-
+using namespace JHMG_ENGINE;
 Game* mainGame;
 
 void awake()
@@ -15,18 +15,18 @@ static float b1 = 0;
 static int status = 0;
 
 // 按钮点击事件
-void onClick(int mouseMessage, jhVector2 position, gameUI* self);
+void onClick(int mouseMessage, Vector2 position, GameUI* self);
 
 int main()
 {
 	// 创建游戏
 	mainGame = new Game;
 	// 设置窗口大小
-	mainGame->setWindowSize(jhVector2(300, 500));
+	mainGame->setWindowSize(Vector2(300, 500));
 	// 设置窗口标题
 	mainGame->setWindowTitle("计算器");
 	// 创建场景
-	gameScene* scene = new gameScene;
+	GameScene* scene = new GameScene;
 	// 设置场景
 	mainGame->setScene(scene);
 	// 设置场景唤醒函数
@@ -38,26 +38,26 @@ int main()
 		for (int j = 0; j < 3; j++)
 		{
 			// 生成位置
-			jhVector2 pos = jhVector2(30 + 60 * j, 250 + 60 * i);
+			Vector2 pos = Vector2(30 + 60 * j, 250 + 60 * i);
 			// 生成按钮
-			gameUI* button = new gameUI(pos, jhVector2(50, 50), "bt.jpg");
+			GameUI* button = new GameUI(pos, Vector2(50, 50), "bt.jpg");
 			// 生成按钮文字
-			gameUIText* text = new gameUIText(to_string(i * 3 + j + 1), pos + jhVector2(5, 5),WHITE,jhVector2(0,25),true);
+			GameUIText* text = new GameUIText(std::to_string(i * 3 + j + 1), pos + Vector2(5, 5),WHITE,Vector2(0,25),true);
 			// 设置按钮点击事件
 			button->mouseAction->setClickFunc(onClick);
 			// 添加按钮到场景
-			scene->addGameUI("button" + to_string(i * 3 + j), button);
+			scene->addGameUI("button" + std::to_string(i * 3 + j), button);
 			// 添加按钮文字到场景
-			scene->addGameUIText(to_string(i * 3 + j), text);
+			scene->addGameUIText(std::to_string(i * 3 + j), text);
 		}
 	}
 
 	//生成0按钮
-	jhVector2 pos = jhVector2(90, 430);
+	Vector2 pos = Vector2(90, 430);
 	//生成按钮
-	gameUI* button_0 = new gameUI(pos, jhVector2(50, 50), "bt.jpg");
+	GameUI* button_0 = new GameUI(pos, Vector2(50, 50), "bt.jpg");
 	//生成按钮文字
-	gameUIText* text_0 = new gameUIText("0", pos + jhVector2(5, 5),WHITE,jhVector2(0,25), true);
+	GameUIText* text_0 = new GameUIText("0", pos + Vector2(5, 5),WHITE,Vector2(0,25), true);
 	//设置按钮点击事件
 	button_0->mouseAction->setClickFunc(onClick);
 	//添加按钮到场景
@@ -71,25 +71,25 @@ int main()
 		// 生成符号
 		const char* con[] = { "+", "-", "×", "÷" };
 		// 生成位置
-		jhVector2 pos = jhVector2(230, 250 + 60 * i);
+		Vector2 pos = Vector2(230, 250 + 60 * i);
 		// 生成按钮
-		gameUI* button = new gameUI(pos, jhVector2(50, 50), "bt.jpg");
+		GameUI* button = new GameUI(pos, Vector2(50, 50), "bt.jpg");
 		// 生成按钮文字
-		gameUIText* text = new gameUIText(con[i], pos + jhVector2(5, 5),WHITE,jhVector2(0,25), true);
+		GameUIText* text = new GameUIText(con[i], pos + Vector2(5, 5),WHITE,Vector2(0,25), true);
 		// 设置按钮点击事件
 		button->mouseAction->setClickFunc(onClick);
 		// 添加按钮到场景
-		scene->addGameUI("control" + to_string(i), button);
+		scene->addGameUI("control" + std::to_string(i), button);
 		// 添加按钮文字到场景
 		scene->addGameUIText(con[i], text);
 
 	}
 	//生成等号按钮
-	pos = jhVector2(150, 430);
+	pos = Vector2(150, 430);
 	//生成按钮
-	gameUI* button = new gameUI(pos, jhVector2(50, 50), "bt.jpg");
+	GameUI* button = new GameUI(pos, Vector2(50, 50), "bt.jpg");
 	//生成按钮文字
-	gameUIText* text = new gameUIText("=", pos + jhVector2(5, 5),WHITE,jhVector2(0,25), true);
+	GameUIText* text = new GameUIText("=", pos + Vector2(5, 5),WHITE,Vector2(0,25), true);
 	//设置按钮点击事件
 	button->mouseAction->setClickFunc(onClick);
 	//添加按钮到场景
@@ -100,11 +100,11 @@ int main()
 	//生成清零按钮
 	
 	//生成位置
-	pos = jhVector2(30, 430);
+	pos = Vector2(30, 430);
 	//生成按钮
-	gameUI* button0 = new gameUI(pos, jhVector2(50, 50), "bt.jpg");
+	GameUI* button0 = new GameUI(pos, Vector2(50, 50), "bt.jpg");
 	//生成按钮文字
-	gameUIText* text0 = new gameUIText("C", pos + jhVector2(5, 5),WHITE,jhVector2(0,25), true);
+	GameUIText* text0 = new GameUIText("C", pos + Vector2(5, 5),WHITE,Vector2(0,25), true);
 	//设置按钮点击事件
 	button0->mouseAction->setClickFunc(onClick);
 	//添加按钮到场景
@@ -114,9 +114,9 @@ int main()
 
 	//生成显示框
 	//生成位置
-	pos = jhVector2(30, 80);
+	pos = Vector2(30, 80);
 	//生成显示框文本
-	gameUIText* text1 = new gameUIText("", pos,BLACK,jhVector2(0,25), true);
+	GameUIText* text1 = new GameUIText("", pos,BLACK,Vector2(0,25), true);
 	//添加显示框到场景
 	scene->addGameUIText("结果", text1);
 
@@ -124,11 +124,11 @@ int main()
 	mainGame->initWindow();
 }
 
-void onClick(int mouseMessage, jhVector2 position, gameUI* self)
+void onClick(int mouseMessage, Vector2 position, GameUI* self)
 {
 	if (mouseMessage == MouseMessage::leftDown)
 	{
-		jhString name = mainGame->getScene()->getName(self);
+		String name = mainGame->getScene()->getName(self);
 		if (name == "button0")
 		{
 			b1 = b1 * 10 + 1;
@@ -223,6 +223,6 @@ void onClick(int mouseMessage, jhVector2 position, gameUI* self)
 			a1 = 0;
 		}
 		// 显示结果
-		mainGame->getScene()->getGameUIText("结果")->text = to_string(b1);
+		mainGame->getScene()->getGameUIText("结果")->text = std::to_string(b1);
 	}
 }
